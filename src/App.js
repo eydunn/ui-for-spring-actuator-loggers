4c796b52cc8949988a38137c778652e11dc869d8
Eyðun Nielsen <eydun.nielsen@globetracker.com>
2023-06-01 09:24:33 +0100

    Listing all endpoints + dropdown with allowed log levels

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    setData(jsonData);
+  };
+
+  useEffect(() => {
+    if (url !== '') {
+      fetchData();
+    }
+  }, [url]);
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleLevelChange = (logger, event) => {
+    const newData = { ...data };
+    newData.loggers[logger].configuredLevel = event.target.value;
+    setData(newData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <input type="text" value={url} onChange={handleUrlChange} placeholder="Enter URL" />
+        {data && (
+            <div>
+              {Object.keys(data.loggers).map((logger) => (
+                  <div key={logger}>
+                    <h2>{logger}</h2>
+                    <select
+                        value={data.loggers[logger].configuredLevel}
+                        onChange={(event) => handleLevelChange(logger, event)}
+                    >
+                      {data.levels.map((level) => (
+                          <option key={level} value={level}>
+                            {level}
+                          </option>
+                      ))}
+                    </select>
+                  </div>
+              ))}
+            </div>
+        )}
+      </div>
   );
 }
 
