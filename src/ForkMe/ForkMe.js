d1c8599cca82279841ef22bc359e169d5ef9768a
Eyðun Nielsen <eydun.nielsen@globetracker.com>
2023-06-02 11:36:59 +0100

    Bugfix - link to correct repo

--- src/ForkMe/ForkMe.js
+++ src/ForkMe/ForkMe.js
@@ -3,7 +3,7 @@ import './ForkMe.css';
 import forkmelogo from './forkme_right_red_aa0000.png';
 
 function ForkMe() {
-    return  <a href="https://github.com/eydunn/true-cost-of-meetings" target="_blank" rel="noopener noreferrer" className="ForkMe">
+    return  <a href="https://github.com/eydunn/spring-actuator-loggers" target="_blank" rel="noopener noreferrer" className="ForkMe">
                 <img src={forkmelogo} alt="Fork me on GitHub" />
             </a>;
 }
