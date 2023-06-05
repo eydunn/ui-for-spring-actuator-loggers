1d7cb7052209f73e0c724d6c18fbf1bad86da588
Eyðun Nielsen <eydun.nielsen@globetracker.com>
2023-06-06 00:34:27 +0100

    Updates

---
+++ precache-manifest.f796db9716859227a3ce94362e48ea0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f16ed7dc8befc840f9",
+    "url": "/ui-for-spring-actuator-loggers/static/css/main.2cb44015.chunk.css"
+  },
+  {
+    "revision": "58f16ed7dc8befc840f9",
+    "url": "/ui-for-spring-actuator-loggers/static/js/main.58f16ed7.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/ui-for-spring-actuator-loggers/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "402c5000f8f5c9d52416",
+    "url": "/ui-for-spring-actuator-loggers/static/js/2.402c5000.chunk.js"
+  },
+  {
+    "revision": "007f5eb284a8b393af2d",
+    "url": "/ui-for-spring-actuator-loggers/static/js/runtime~main.007f5eb2.js"
+  },
+  {
+    "revision": "fd71201371613b04bcffdecb389b3d54",
+    "url": "/ui-for-spring-actuator-loggers/index.html"
+  }
+];
